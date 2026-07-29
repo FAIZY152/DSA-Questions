71350da73f3ba42ac797d5af9f1ded8acf72b81a
Dev2-marketasa <dev2@marketasa.com>
2026-07-29 12:07:10 +0500

    solve-check-palindrom-qs

---
+++ Day04/Palidrom.js
@@ -0,0 +1,49 @@
+// function Palindrom(x) {
+//     if (x.length <= 1) {
+//         return false
+//     }
+    
+//     let revrs = x.toString().split("").reverse().join("")
+//     let palidoromNm = Number(revrs)
+
+//     if (palidoromNm === x) {
+//         return true
+//     }
+//     return false
+    
+// }
+// let x = -121
+// let res = Palindrom(x)
+// console.log(res);
+
+
+
+
+function Palindrom(x) {
+
+    // Negative numbers are not palindromes
+    if (x < 0) {
+        return false;
+    }
+
+    let original = x;
+    let reverse = 0;
+
+    while (x > 0) {
+        let digit = x % 10;
+        reverse = reverse * 10 + digit;
+        x = Math.floor(x / 10);
+    }
+
+    // return original === reverse;
+    if (original === reverse) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(Palindrom(121));   // true
+console.log(Palindrom(432));   // false
+console.log(Palindrom(1221));  // true
+console.log(Palindrom(-121));  // false
