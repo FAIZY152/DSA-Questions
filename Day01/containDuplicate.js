c69738849f344c11e7db54f4ec1d3d1492013971
FAIZY152 <fayazjani7576@gmail.com>
2026-08-04 20:46:12 +0500

    handle-contain-duplicates

--- Day01/containDuplicate.js
+++ Day01/containDuplicate.js
@@ -27,7 +27,7 @@ console.log(ContainsDuplicate(arr)
 
 function ContainsDuplicate(arr) {
    let sortedArr = arr.sort((a, b) => a - b);
-console.log(sortedArr);
+    console.log(sortedArr);
 
     for (let i = 0; i < sortedArr.length - 1; i++) {
         if (sortedArr[i]=== sortedArr[i + 1]) {
